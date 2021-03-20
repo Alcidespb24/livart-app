0453f892a29b435ffd5460f0364fcef639b1dc6b
Alcides <alcidesperezb@gmail.com>
2021-03-19 20:38:56 -0400

    major changes, check before merging

--- lib/screens/dashboards/djDashboard.dart
+++ lib/screens/dashboards/djDashboard.dart
@@ -13,78 +13,77 @@ class _DjDashboardState extends State<DjDashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Stack(
         children: <Widget>[
-          Container(
-            decoration: globalTheme.backgroundGradient,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                globalTheme.locationIcon,
-                Container(
-                  margin: EdgeInsets.only(bottom: 5, top: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        'Tell us where you are: ',
+            Container(
+              decoration: globalTheme.backgroundGradient,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  globalTheme.locationIcon,
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Tell us where you are: ',
+                          style: TextStyle(
+                            fontSize: 25,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ), //User
+                  SizedBox(height: 10),
+                  Container(
+                    width: 250,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: globalTheme.buttonColor4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(14.0),
+                        ),
+                        elevation: 1,
+                      ),
+                      onPressed: () {
+                        // Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+                      },
+                      child: Text(
+                        'Enable Location',
                         style: TextStyle(
-                          fontSize: 25,
+                          fontSize: 15,
                         ),
                       ),
-                    ],
-                  ),
-                ), //User
-                SizedBox(height: 10),
-                Container(
-                  height: 38,
-                  width: 250,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: globalTheme.buttonColor4,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(14.0),
-                      ),
-                      elevation: 1,
-                    ),
-                    onPressed: () {
-                      // Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
-                    },
-                    child: Text(
-                      'Enable Location',
-                      style: TextStyle(
-                        fontSize: 15,
-                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 15),
-                Container(
-                  width: 200,
-                  height: 35,
-                  child: TextFormField(
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Minimum price per request:',
-                      hintStyle: TextStyle(fontSize: 12),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                        borderSide: BorderSide(color: Colors.white),
+                  SizedBox(height: 10),
+                  Container(
+                    width: 200,
+                    height: 35,
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Minimum price per request:',
+                        hintStyle: TextStyle(fontSize: 12),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
                       ),
-                      focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                     ),
-                    keyboardType: TextInputType.number,
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
         ],
       ),
     );
