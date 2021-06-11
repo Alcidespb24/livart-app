7b9a51a7d0a869d9ba985678fd13b26d97644134
Alcidespb24 <alcidesperezb@gmail.com>
2021-06-11 18:03:18 -0400

    minor UI changes

--- lib/screens/places_api/SearchListWidget.dart
+++ lib/screens/places_api/SearchListWidget.dart
@@ -1,4 +1,3 @@
-import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_app/services/ProvidersService.dart';
@@ -10,30 +9,39 @@ class SearchListWidget extends ConsumerWidget {
     return Stack(
       children: <Widget>[
         if (list.length != 0)
-          Container(
-            height: double.infinity,
-            child: Scrollbar(
-              isAlwaysShown: true,
-              child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                clipBehavior: Clip.hardEdge,
-                itemCount: list.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    onTap: () {},
-                    leading: Icon(
-                      Icons.location_pin,
-                      color: Colors.red,
+          Divider(
+            indent: 25,
+            endIndent: 25,
+            color: Colors.white.withOpacity(0.5),
+            height: 35,
+          ),
+        Container(
+          margin: EdgeInsets.only(top: 25),
+          height: double.infinity,
+          child: Scrollbar(
+            isAlwaysShown: false,
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              clipBehavior: Clip.hardEdge,
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  onTap: () {},
+                  leading: Icon(
+                    Icons.location_pin,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    list[index].name,
+                    style: TextStyle(
+                      color: Colors.white,
                     ),
-                    title: Text(
-                      list[index].name,
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             ),
           ),
+        ),
       ],
     );
   }
